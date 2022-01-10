6b1a3dbbe1e4d72054f0b55fa9cf38cb2faffb8c
danil <danilsyaharihardjo@gmail.com>
2022-01-10 12:37:09 +0700

    revisi

--- alternate-case.js
+++ alternate-case.js
@@ -1,12 +1,9 @@
 function alternateCase(text){
     textArr = []
    for (let i = 0; i < text.length; i++){
-    //    console.log(text.charAt(i))
         if (text.charAt(i) == text.charAt(i).toUpperCase()){
-            // console.log(text.charAt(i), "besar")
             textArr[i] = text.charAt(i).toLowerCase()
         }else{
-            // console.log(text.charAt(i), "kecil")
             textArr[i] = text.charAt(i).toUpperCase()
         }
    }
